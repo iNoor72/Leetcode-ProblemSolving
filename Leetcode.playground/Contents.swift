0bfbb9d1f8ae24b38df68c247f7330358633d292
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-10-06 01:19:29 +0200

    Added solution for problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/

--- Leetcode.playground/Contents.swift
+++ Leetcode.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Foundation
 
-//Problem #1: 
+//Problem #1: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var numbersList: [Int] = [Int](-100...100)
+    var resultArray = [Int]()
+    for num in numbersList {
+        if nums.contains(num) {
+            resultArray.append(num)
+        }
+    }
+    
+    nums = resultArray
+    return nums.count
+}
+
+var inputIntArray = [1,1,2]
+removeDuplicates(&inputIntArray)
+print(inputIntArray)
